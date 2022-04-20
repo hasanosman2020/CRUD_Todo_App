73e3c377af468659c0221a3c23b2d546e6b4a1b6
hasanosman2020 <hasan79.hoa@gmail.com>
2022-04-20 05:30:52 +0100

    DOCS: create functions acceptData, createTasks, resetForm and render tasks on screen using localStorage.getItem

--- main.js
+++ main.js
@@ -21,10 +21,70 @@ let formValidation = () => {
   } else {
     console.log('success')
     msg.innerHTML = ' '
-    //acceptData()
+
+    acceptData()
+
+    //function to close the modal when user clicks on the button labelled 'Add
+    add.setAttribute('data-bs-dismiss', 'modal')
+    add.click()
+    /*
+    (() => {
+      add.setAttribute('data-bs-dismiss', '')
+    })()*/
   }
 }
+
+//Empty array tp collect the user data from the acceptData() function below
+let data = []
+
+//Function to collect user data
+let acceptData = () => {
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: textarea.value
+  })
+
+  localStorage.setItem('data', JSON.stringify(data))
+
+  console.log(data)
+
+  createTasls()
+}
+
+//Function to creat new tasks
+let createTasks = () => {
+  tasks.innerHTML = ' '
+  data.map((x, y) => {
+    return (tasks.innerHTML += `<div id = ${y}>
+            <span class = 'fw-bold'>${x.text}</span>
+            <span class = 'small text-secondary'>${x.date}</span>
+            <p>${x.description}</p>
+
+            <span class = 'options'>
+            <i onClick = 'editTask(this)' data-bs-toggle = 'modal' data-bs-target = 'form' class = 'fas fa-edit'></i>
+            <i onClick = 'deleteTask(this):createTasks()' class = 'fas fa-trash'></i>
+            </span>
+            
+            </div>`)
+  })
+  resetForm()
+}
+
+let resetForm = () => {
+  textInput.value = ''
+  dateInput.value = ''
+  textarea.value = ''
+}
+
+;(() => {
+  data = JSON.parse(localStorage.getItem('data')) || []
+  console.log(data)
+  createTasks()
+})()
+
 /*
+
     add.setAttribute('data-bs-dismiss', 'modal')
 
     add.click()
